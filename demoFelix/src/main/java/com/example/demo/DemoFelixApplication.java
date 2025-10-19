11850bc2fe63950b109eea20c172e2e0220b8131
SrFPerez <felixperezviegas@gmail.com>
2025-10-19 09:37:06 +0200

    Primera subida proyecto spring

---
+++ demoFelix/src/main/java/com/example/demo/DemoFelixApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFelixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFelixApplication.class, args);
+		
+		
+		System.out.println("Prueba de funcionamiento!! HOLA MUNDO!!");
+		
+		
+	}
+
+}
